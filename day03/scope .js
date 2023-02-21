daae414fd4836ee9682d944aca93b82995782413
ANIKET . S . BHADRE <122332696+aniketbhadre01@users.noreply.github.com>
2023-02-21 09:38:54 +0530

    Add files via upload

---
+++ day03/scope .js
@@ -0,0 +1,18 @@
+var rseult;
+function sum(a, b) {
+  //function scope
+  var result = a + b;
+}
+sum(4, 5);
+//block scope
+//console.log(result);
+{
+    //block scope
+}
+if(true){
+    //block scope
+}
+ for (var i=1; i<=10;i++){
+    var z=
+
+ }
